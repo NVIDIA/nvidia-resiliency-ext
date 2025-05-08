8ec09491034babe5a8a877d15fe4c4bb1c9d55f9
Hexin Wang <hexinw@nvidia.com>
2025-05-08 00:07:26 -0700

    Enhanced InJob restart in RankMonitorServer and launcher:
    - Modified read_obj_from_ipc_stream to raise exceptions instead of returning None,
      improving error handling clarity and robustness.
    - Updated RankMonitorServer to handle launcher IPC connections, including explicit
      connection closure on "close_worker_ipc_connection" message.
    - Added launcher IPC server to RankMonitorServer to receive commands from
      the launcher.
    - Enhanced LocalElasticAgent to send close messages to rank monitors before shutting
      down workers, ensuring graceful connection closure.
    - Updated RankMonitorStateMachine to allow FINALIZED state transitions in FINALIzED state.
      RankMonitorServer can receive SIGNAL before handling worker IPC connection lost.
    - Adjusted unit tests to reflect valid signal handling in FINALIZED state.
    - Added proper cleanup of launcher IPC socket file in RankMonitorServer shutdown.
    - Added RankMonitorServer unit test.

--- tests/fault_tolerance/unit/test_rank_monitor_server.py
+++ tests/fault_tolerance/unit/test_rank_monitor_server.py
@@ -4,7 +4,7 @@ import socket
 import tempfile
 import time
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import torch
 import torch.multiprocessing
@@ -20,10 +20,7 @@ from nvidia_resiliency_ext.fault_tolerance.data import (
     SectionMsg,
 )
 from nvidia_resiliency_ext.fault_tolerance.rank_monitor_server import RankMonitorServer
-from nvidia_resiliency_ext.fault_tolerance.utils import (
-    read_obj_from_ipc_stream,
-    write_obj_to_ipc_stream,
-)
+from nvidia_resiliency_ext.fault_tolerance.utils import write_obj_to_ipc_stream, read_obj_from_ipc_stream
 
 
 class TestRankMonitorServer(unittest.TestCase):
