98d0ed47dc2d968ce71e496325bd1c442e188ffa
Russell Hewett <rhewett@nvidia.com>
2025-05-13 14:46:01 -0700

    Make ft-launcher usage consistent everywhere

--- examples/fault_tolerance/run_inprocess_injob_example.sh
+++ examples/fault_tolerance/run_inprocess_injob_example.sh
@@ -15,26 +15,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This script demonstrates the use of in-process and fault-tolerance (FT) packages within 
-# a single PyTorch-based workload. The in-process package detects rank exceptions and crashes 
+# This script demonstrates the use of in-process and fault-tolerance (FT) packages within
+# a single PyTorch-based workload. The in-process package detects rank exceptions and crashes
 # and restarts the training using the remaining worker processes with minimal overhead.
 # The FT package is used to detect hangs and can restart training processes if needed.
-# NOTE: The in-process package has monitoring capabilities that can also be used. 
+# NOTE: The in-process package has monitoring capabilities that can also be used.
 # Please refer to the in-process package documentation for details.
 #
-# When using `ft_launcher --restart-policy=min-healthy --nnodes=2:3`, fault tolerance 
-# restarts the training when the number of healthy agents (agents with all ranks alive) 
+# When using `ft_launcher --restart-policy=min-healthy --nnodes=2:3`, fault tolerance
+# restarts the training when the number of healthy agents (agents with all ranks alive)
 # falls below 2.
 #
 # Simulated sequence:
 # - The minimum number of nodes (launcher instances, agents) is set to 2, and the maximum to 3.
 # - Training starts with 3 agents, each having 2 ranks; hence, the world size is 6.
-# - A simulated exception in rank 5 at iteration 300 is handled with an in-process restart, 
+# - A simulated exception in rank 5 at iteration 300 is handled with an in-process restart,
 #   reusing all training processes to resume training.
-# - Rank 5 is terminated with SIGKILL at iteration 500. The in-process mechanism handles the failure, 
+# - Rank 5 is terminated with SIGKILL at iteration 500. The in-process mechanism handles the failure,
 #   but since the rank process is lost, training resumes with the remaining 5 processes.
 # - Rank 4 is terminated with SIGKILL, further reducing the world size to 4 processes.
-# - Rank 3 is terminated, reducing the world size to 3. At this point, the FT package 
+# - Rank 3 is terminated, reducing the world size to 3. At this point, the FT package
 #   detects that training has failed because only one healthy (fully utilized) node remains.
 # - The FT package restarts all 6 training processes, and training is resumed.
 # - A simulated hang is detected by the fault-tolerance package, triggering another full restart.
@@ -48,12 +48,12 @@ THIS_SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 WORKER_SCRIPT="${THIS_SCRIPT_DIR}/in_job_and_in_process_example.py"
 
 COMMON_LAUNCHER_ARGS="--nnodes=2:3 --max-restarts=2 --nproc_per_node=2 --rdzv_backend=c10d --rdzv_endpoint=127.0.0.1:29500 --monitor-interval=5"
-COMMON_FT_ARGS="--ft-param-log_level=DEBUG --ft-param-rank_heartbeat_timeout=12 --ft-param-initial_rank_heartbeat_timeout=12 --restart-policy=min-healthy"
+COMMON_FT_ARGS="--ft-param-log-level=DEBUG --ft-param-rank-heartbeat-timeout=12 --ft-param-initial-rank-heartbeat-timeout=12 --ft-param-restart-policy=min-healthy"
 
 # WAR: this example currently does not work with the NIC monitor
-COMMON_FT_ARGS="${COMMON_FT_ARGS} --ft_param_enable_nic_monitor=False"
+COMMON_FT_ARGS="${COMMON_FT_ARGS} --ft-param-enable-nic-monitor=False"
 
-# Avoid c10d log clutter with "(...) TCPStore.cpp:122] [c10d] sendBytes failed (...)" 
+# Avoid c10d log clutter with "(...) TCPStore.cpp:122] [c10d] sendBytes failed (...)"
 # when terminating a hung workload and the TCP Store hosting rank is killed.
 export TORCH_CPP_LOG_LEVEL=ERROR
 
