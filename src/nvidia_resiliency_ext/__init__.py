e710ddc44ce1a2bb0b6c0b52390f6d328dd76b7c
Hexin Wang <hexinw@nvidia.com>
2025-07-18 02:19:57 -0700

    Lint fix.

--- src/nvidia_resiliency_ext/__init__.py
+++ src/nvidia_resiliency_ext/__init__.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("nvidia-resiliency-ext")
