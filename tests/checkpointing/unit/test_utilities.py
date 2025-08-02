4535e0e19e613c62676cc3a16143f9a5f8575808
Diego Pontoriero <74719+diegs@users.noreply.github.com>
2025-08-01 23:42:18 -0700

    checkpointing: fix error propagation and add test
    Fixes error propagation during checkpoint saving
    by using torch's DistWrapper to send the exception
    to the coordinator instead of killing the process
    early. Also fixes call path to ensure necessary
    operations happen in finally blocks.
    
    Adds a test and slightly refactors the multiprocessing
    invocation to allow overriding the open() function
    with one that raises an exception. This enables the
    test. In the future we will factor our the filesystem
    operations into a delegate so it can be more easily
    mocked.

--- tests/checkpointing/unit/test_utilities.py
+++ tests/checkpointing/unit/test_utilities.py
@@ -69,6 +69,7 @@ class Utils:
             and Utils.world_size != torch.distributed.get_world_size()
         ):
             torch.distributed.destroy_process_group()
+            Utils.inited = False
 
         if rank is None:
             Utils.rank = int(os.environ['LOCAL_RANK'])
@@ -78,7 +79,7 @@ class Utils:
             Utils.rank = rank
 
 
-class TestModel(torch.nn.Module):
+class Model(torch.nn.Module):
     def __init__(self, size: Tuple, ntensor: int) -> None:
         super().__init__()
         for i in range(ntensor):
