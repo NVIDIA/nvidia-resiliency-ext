9ceb09f328631e4edc89a9929455d3706aa78064
Diego Pontoriero <74719+diegs@users.noreply.github.com>
2025-09-02 18:15:25 -0700

    fix merge conflict

--- tests/checkpointing/unit/conftest.py
+++ tests/checkpointing/unit/conftest.py
@@ -41,7 +41,7 @@ def tmp_path_dist_ckpt(tmp_path_factory) -> Path:
         yield tmp_dir
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def async_queue():
     async_queue = AsyncCallsQueue()
     yield async_queue
