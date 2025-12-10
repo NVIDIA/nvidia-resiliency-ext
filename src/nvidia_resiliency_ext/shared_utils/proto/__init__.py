ae388a6cb2284e94015ff1f2a1f39cc398bd1e0c
Namit Dhameja <ndhameja@nvidia.com>
2025-12-10 12:43:27 -0800

    remove version from sub pkg init files

--- src/nvidia_resiliency_ext/shared_utils/proto/__init__.py
+++ src/nvidia_resiliency_ext/shared_utils/proto/__init__.py
@@ -13,10 +13,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("nvidia-resiliency-ext")
-except PackageNotFoundError:
-    __version__ = "unknown"
