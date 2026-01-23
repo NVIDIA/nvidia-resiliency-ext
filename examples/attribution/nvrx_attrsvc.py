d2ee2e8e28fb90a2427c70a7e1c490798f8e5acd
helisha <helisha@nvidia.com>
2026-01-23 20:49:32 +0200

    nvrx attrsvc new logsage ver

--- examples/attribution/nvrx_attrsvc.py
+++ examples/attribution/nvrx_attrsvc.py
@@ -556,8 +556,8 @@ def create_app(cfg: Settings) -> FastAPI:
                                 "ts_current_time": round(datetime.now().timestamp() * 1000),
                             }
 
-                            if HAS_NVDATAFLOW:# and cfg.NVDATAFLOW_PROJECT:
-                                result = nv_post(data=data, project="coreai_resiliency_osiris")#cfg.NVDATAFLOW_PROJECT)
+                            if HAS_NVDATAFLOW and cfg.NVDATAFLOW_PROJECT:
+                                result = nv_post(data=data, project=cfg.NVDATAFLOW_PROJECT)
                             else:
                                 if HAS_NVDATAFLOW:
                                     logger.error("nvdataflow index is missing")
